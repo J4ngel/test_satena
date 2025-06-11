8905cb455156a45c0f86e6de6a819a67bc90ebb9
Jaime <andres_angel@live.com.ar>
2025-06-11 15:30:47 -0500

    feat: :sparkles: Add NavBar component

---
+++ src/app/ui/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">Test Satena</div>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/" className="text-white hover:text-gray-300">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="text-white hover:text-gray-300">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="#contact" className="text-white hover:text-gray-300">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link href="/login" className="text-white hover:text-gray-300">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
